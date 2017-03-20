d10ce3c1e8b3324bf94fbadd31c0dfcefadb196b
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-20 16:04:17 +0100

    Base js for module

--- dist/js/api-alarm-integration.dev.js
+++ dist/js/api-alarm-integration.dev.js
@@ -3,11 +3,44 @@ ApiAlarmIntegration.FetchAlarms = (function ($) {
 
     function FetchAlarms() {
         $('[data-api-alarm-integration="load"]').each(function (index, element) {
-            console.log(element);
+            this.init(element);
+            $(element).removeAttr('data-api-alarm-integration');
         }.bind(this));
     }
 
+    /**
+     * Init alarms for each module/widget
+     * @param  {object} element
+     * @return {void}
+     */
+    FetchAlarms.prototype.init = function(element) {
+        var $element = $(element);
+        var apiUrl = $element.attr('data-alarm-api');
+        var perPage = $element.attr('data-alamrs-per-page');
+        var currentPage = $element.attr('data-alamrs-current-page');
 
+        this.loadAlarms(element, apiUrl, perPage, currentPage);
+    };
+
+    /**
+     * Load alarms
+     * @param  {object} element
+     * @param  {string} apiUrl
+     * @param  {int}    perPage
+     * @param  {int}    currentPage
+     * @return {void}
+     */
+    FetchAlarms.prototype.loadAlarms = function(element, apiUrl, perPage, currentPage) {
+        var requestUrl = apiUrl + 'wp/v2/alarm';
+        var data = {
+            per_page: perPage,
+            page: currentPage + 1
+        };
+
+        $.getJSON(requestUrl, data, function (response) {
+            console.log(response);
+        });
+    };
 
     return new FetchAlarms();
 
--- templates/module/modularity-mod-alarms.php
+++ templates/module/modularity-mod-alarms.php
@@ -2,7 +2,7 @@
     <?php if (!$module->hideTitle && !empty($module->post_title)) : ?>
         <h4 class="box-title"><?php echo apply_filters('the_title', $module->post_title); ?></h4>
     <?php endif; ?>
-    <ul data-api-alarm-integration="load">
+    <ul data-api-alarm-integration="load" data-alamrs-per-page="<?php echo get_field('alarms_per_page', $module->ID); ?>" data-alamrs-current-page="0" data-alarm-api="<?php echo trailingslashit(get_field('api_url', $module->ID)); ?>">
         <li>
             <h5>{{alarm.title}}</h5>
         </li>
