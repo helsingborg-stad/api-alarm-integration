48ba16f56403bc7d604fd85309443a800c9494df
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-21 13:06:27 +0100

    ”Load more” button

--- source/js/Fetch/Fetch.js
+++ source/js/Fetch/Fetch.js
@@ -1,5 +1,3 @@
-var ApiAlarmIntegration = {};
-
 ApiAlarmIntegration = ApiAlarmIntegration || {};
 ApiAlarmIntegration.FetchAlarms = (function ($) {
 
@@ -93,64 +91,3 @@ ApiAlarmIntegration.FetchAlarms = (function ($) {
     return new FetchAlarms();
 
 })(jQuery);
-
-ApiAlarmIntegration = ApiAlarmIntegration || {};
-ApiAlarmIntegration.Helper = ApiAlarmIntegration.Helper || {};
-
-ApiAlarmIntegration.Helper.Template = (function () {
-
-    var _templates = [];
-
-    function Template() {
-        this.getTemplates();
-    }
-
-    Template.prototype.render = function(key, data) {
-        // Get template html
-        var template = _templates[key];
-
-        // Replace template strings
-        template = template.replace(/{{\s*([\w\.]+)\s*}}/g, function($1, $2) {
-            var value = data;
-            var objKey = $2.split('.');
-
-            for (var i = 0; i < objKey.length; i++) {
-                value = value[objKey[i]];
-            }
-
-            return value;
-        });
-
-        // Handle if-statements
-        template = template.replace(/{%\s*if(.*)\s*%}([\s\S]*?){%\s*endif\s*%}/gm, function ($1, $2, $3) {
-            if (!eval($2)) {
-                return '';
-            }
-
-            return $3;
-        });
-
-        // Return the new html
-        return template;
-    };
-
-    /**
-     * Store templates in _templates array and removes it from the DOM
-     * @return {void}
-     */
-    Template.prototype.getTemplates = function() {
-        var templateElements = document.querySelectorAll('[data-template]');
-
-        for (var i = 0; i < templateElements.length; i++) {
-            var el = templateElements[i];
-            var key = el.getAttribute('data-template');
-            var html = el.outerHTML.replace(/data-template="([^\"]*)"/ig, '');
-
-            _templates[key] = html;
-            el.parentElement.removeChild(el);
-        }
-    };
-
-    return new Template();
-
-})();
--- templates/module/modularity-mod-alarms.php
+++ templates/module/modularity-mod-alarms.php
@@ -3,41 +3,58 @@
         <h4 class="box-title"><?php echo apply_filters('the_title', $module->post_title); ?></h4>
     <?php endif; ?>
 
-    <ul class="accordion accordion-list accordion-list-small" data-api-alarm-integration="load" data-alamrs-per-page="<?php echo get_field('alarms_per_page', $module->ID); ?>" data-alamrs-current-page="0" data-alarm-api="<?php echo trailingslashit(get_field('api_url', $module->ID)); ?>">
-        <li style="padding:20px 0;" data-alarms-loading>
-            <div class="loading">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+    <div class="box-content filters" style="border-bottom:0;">
+        <div class="grid-table no-padding">
+            <div class="grid-auto">
+                <input type="search" data-alarm-filter="freetext" placeholder="<?php _e('Search…', 'api-alarm-integration'); ?>">
             </div>
-        </li>
-        <li class="accordion-section no-padding" data-template="api-alarm-integration-row">
-            <input type="radio" name="active-section" id="accordion-section-{{ id }}">
-            <label class="accordion-toggle link-item" for="accordion-section-{{ id }}">
-                {{ place }}: {{ title.rendered }}
-            </label>
-            <div class="accordion-content">
-                <table>
-                    <tr>
-                        <td><strong>Tidpunk:</strong></td><td>{{ date }}</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Händelse:</strong></td><td>{{ title.rendered }}</td>
-                        <td><strong>Larmnivå:</strong></td><td>{{ type }}</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Adress:</strong></td><td>{{ address }}, {{ place }}</td>
-                        <td><strong>Station:</strong></td><td> {{ station.title }}</td>
-                    </tr>
-                </table>
+            <div class="grid-auto">
+                <select data-alarm-filter="place">
+                    <option value="Helsingborg">Helsingborg</option>
+                </select>
+            </div>
+            <div class="grid-fit-content">
+                <button class="btn btn-primary">Sök</button>
+            </div>
+        </div>
+    </div>
 
-                {% if data.content.rendered.length %}
-                <article>
-                    {{ content.rendered }}
-                </article>
-                {% endif %}
-            </div>
-        </li>
-    </ul>
+    <div class="box-content no-padding">
+        <ul class="accordion accordion-list accordion-list-small alarms-container" data-api-alarm-integration="load" data-alamrs-per-page="<?php echo get_field('alarms_per_page', $module->ID); ?>" data-alamrs-current-page="0" data-alarm-api="<?php echo trailingslashit(get_field('api_url', $module->ID)); ?>">
+            <li style="padding:20px 0;" data-template="api-alarm-integration-loading" data-api-alarms-load-more>
+                <div class="loading">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </li>
+            <li class="accordion-section no-padding" data-template="api-alarm-integration-row">
+                <input type="radio" name="active-section" id="accordion-section-{{ id }}">
+                <label class="accordion-toggle link-item" for="accordion-section-{{ id }}">{{ place }}: {{ title.rendered }}</label>
+                <div class="accordion-content">
+                    <table>
+                        <tr>
+                            <td><strong><?php _e('Time', 'api-alarm-integration'); ?>:</strong></td><td>{{ date }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong><?php _e('Incident', 'api-alarm-integration'); ?>:</strong></td><td>{{ title.rendered }}</td>
+                            <td><strong><?php _e('Level', 'api-alarm-integration'); ?>:</strong></td><td>{{ type }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong><?php _e('Address', 'api-alarm-integration'); ?>:</strong></td><td>{{ address }}, {{ place }}</td>
+                            <td><strong><?php _e('Station', 'api-alarm-integration'); ?>:</strong></td><td> {{ station.title }}</td>
+                        </tr>
+                    </table>
+
+                    {% if data.content.rendered.length %}
+                    <article>
+                        {{ content.rendered }}
+                    </article>
+                    {% endif %}
+                </div>
+            </li>
+            <li data-template="api-alarm-integration-load-more" style="padding:10px;" data-api-alarms-load-more><button data-action="api-alarm-integration-load-more" class="btn btn-block"><?php _e('Show more alarms', 'api-alarm-integration'); ?></button></li>
+        </ul>
+    </div>
 </div>
