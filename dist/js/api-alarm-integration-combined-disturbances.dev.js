a03c94e6fe41fc1690d0589e6cd257db325dda02
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-06-07 13:31:27 +0200

    change from jquery getJson method to ajax

--- dist/js/api-alarm-integration-combined-disturbances.dev.js
+++ dist/js/api-alarm-integration-combined-disturbances.dev.js
@@ -21,57 +21,69 @@ window.addEventListener('load', function () {
         disturbances.inited = true;
     }
 
-    $.getJSON(requestUrl, data, function (response) {
-        if (disturbances.output_small_active) {
-            $.each(response.small, function (index, item) {
-                if ($('#disturbance-' + item.ID).length > 0) {
-                    return;
+    $.ajax({
+            type: "GET",
+            url: requestUrl,
+            cache: false,
+            dataType: "json",
+            data : data,
+            crossDomain: true,
+            success: function( response ) {
+                if (disturbances.output_small_active) {
+                    $.each(response.small, function (index, item) {
+                        if ($('#disturbance-' + item.ID).length > 0) {
+                            return;
+                        }
+
+                        var $notice = $('\
+                            <div class="notice notice-disturbance notice-fullwidth info" id="disturbance-' + item.ID + '" style="display:none;">\
+                                <div class="container">\
+                                    <div class="grid grid-table">\
+                                        <div class="grid-auto">\
+                                            <i class="pricon pricon-notice-info"></i> <strong>' + item.post_title + '</strong>\
+                                        </div>\
+                                        <div class="grid-fit-content">\
+                                            <button type="button" class="btn btn-sm btn-contrasted" data-action="toggle-notice-content">' + disturbances.more_info + '</button>\
+                                        </div>\
+                                    </div>\
+                                    <div class="grid notice-content" style="display:none;">\
+                                        <div class="grid-md-12">' + item.post_content + '</div>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        ').prependTo(disturbances.output_small).slideDown();
+                    });
                 }
 
-                var $notice = $('\
-                    <div class="notice notice-disturbance notice-fullwidth info" id="disturbance-' + item.ID + '" style="display:none;">\
-                        <div class="container">\
-                            <div class="grid grid-table">\
-                                <div class="grid-auto">\
-                                    <i class="pricon pricon-notice-info"></i> <strong>' + item.post_title + '</strong>\
-                                </div>\
-                                <div class="grid-fit-content">\
-                                    <button type="button" class="btn btn-sm btn-contrasted" data-action="toggle-notice-content">' + disturbances.more_info + '</button>\
-                                </div>\
-                            </div>\
-                            <div class="grid notice-content" style="display:none;">\
-                                <div class="grid-md-12">' + item.post_content + '</div>\
-                            </div>\
-                        </div>\
-                    </div>\
-                ').prependTo(disturbances.output_small).slideDown();
-            });
-        }
+                if (disturbances.output_big_active) {
+                    $.each(response.big, function (index, item) {
+                        if ($('#disturbance-' + item.ID).length > 0) {
+                            return;
+                        }
 
-        if (disturbances.output_big_active) {
-            $.each(response.big, function (index, item) {
-                if ($('#disturbance-' + item.ID).length > 0) {
-                    return;
+                        var $notice = $('\
+                            <div class="notice notice-disturbance notice-lg notice-fullwidth warning" id="disturbance-' + item.ID + '" style="display:none;">\
+                                <div class="container">\
+                                    <div class="grid grid-table">\
+                                        <div class="grid-auto">\
+                                            <i class="pricon pricon-notice-warning"></i> <strong>' + item.post_title + '</strong>\
+                                        </div>\
+                                        <div class="grid-fit-content">\
+                                            <button type="button" class="btn btn-sm btn-contrasted" data-action="toggle-notice-content">' + disturbances.more_info + '</button>\
+                                        </div>\
+                                    </div>\
+                                    <div class="grid notice-content" style="display:none;">\
+                                        <div class="grid-md-12">' + item.post_content + '</div>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        ').prependTo(disturbances.output_big).slideDown();
+                    });
                 }
+            },
+            error: function( response, status, error) {
+                console.log('API Alarm Integration plugin: Request failed!');
+            }
+        });
 
-                var $notice = $('\
-                    <div class="notice notice-disturbance notice-lg notice-fullwidth warning" id="disturbance-' + item.ID + '" style="display:none;">\
-                        <div class="container">\
-                            <div class="grid grid-table">\
-                                <div class="grid-auto">\
-                                    <i class="pricon pricon-notice-warning"></i> <strong>' + item.post_title + '</strong>\
-                                </div>\
-                                <div class="grid-fit-content">\
-                                    <button type="button" class="btn btn-sm btn-contrasted" data-action="toggle-notice-content">' + disturbances.more_info + '</button>\
-                                </div>\
-                            </div>\
-                            <div class="grid notice-content" style="display:none;">\
-                                <div class="grid-md-12">' + item.post_content + '</div>\
-                            </div>\
-                        </div>\
-                    </div>\
-                ').prependTo(disturbances.output_big).slideDown();
-            });
-        }
-    });
 });
