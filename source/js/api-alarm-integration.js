4a1d4582c34fe8ed8a8b495f8bf3f9ede9f3625c
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-11-03 11:04:00 +0100

    Rewrote alarm module in pure js and cleaned up entry file

--- source/js/api-alarm-integration.js
+++ source/js/api-alarm-integration.js
@@ -1,2 +1,6 @@
-import List from './Components/List';
 import ObjectByString from './Helper/Object';
+import AlarmModule from './api-alarm-module';
+
+document.querySelectorAll('[data-api-alarms]').forEach((el) => {
+    const AlarmInstance = new AlarmModule(el);
+})
