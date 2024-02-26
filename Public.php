2e8cb55e9eec43024d3abab2b020d2661b440d67
Thor Brink <thor.brink@helsingborg.se>
2024-02-26 06:35:40 +0000

    fix: replace global blade service

--- Public.php
+++ Public.php
@@ -2,23 +2,18 @@
 
 // Public functions
 
-use HelsingborgStad\GlobalBladeService\GlobalBladeService;
+use ComponentLibrary\Init;
 
 if (!function_exists('disturbance_render_blade_view')) {
     function disturbance_render_blade_view($view, $data = [], $compress = true)
     {
-        $bladeEngine = GlobalBladeService::getInstance([
-            APIALARMINTEGRATION_MODULE_VIEW_PATH,
-        ]);
+        $viewPath = APIALARMINTEGRATION_MODULE_VIEW_PATH;
+        $componentLibrary = new Init([]);
+        $bladeEngine = $componentLibrary->getEngine();
+        $data = array_merge( $data, array('errorMessage' => false) );
 
         try {
-            $markup = $bladeEngine->makeView(
-                $view,
-                array_merge(
-                    $data,
-                    array('errorMessage' => false)
-                )
-            )->render();
+            $markup = $bladeEngine->makeView( $view, $data, [], $viewPath )->render();
         } catch (\Throwable $e) {
             $markup .= '<pre style="border: 3px solid #f00; padding: 10px;">';
             $markup .= '<strong>' . $e->getMessage() . '</strong>';
