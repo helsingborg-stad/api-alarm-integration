b62cbfc44ff8552e7351095a26722761f4666be5
GitHub Action <>
2025-05-19 07:17:37 +0000

    build: update package file versions to 4.0.10

--- api-alarm-integration.php
+++ api-alarm-integration.php
@@ -4,7 +4,7 @@
  * Plugin Name:       API Alarm Integration
  * Plugin URI:        https://github.com/helsingborg-stad/api-alarm-integration
  * Description:       Shows alarms from API Alarm Manager
- * Version: 4.0.9
+ * Version: 4.0.10
  * Author:            Kristoffer Svanmark
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
