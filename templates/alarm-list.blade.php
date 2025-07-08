3ca907facafb4a80a1e1190dd8393b6894f24391
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-07-08 16:40:04 +0200

    fix: update design mock

--- templates/alarm-list.blade.php
+++ templates/alarm-list.blade.php
@@ -10,7 +10,7 @@
         @endtypography
         @endif
 
-        @paper(['classList' => ['c-paper--alarm-list']])
+        
         @collection([
             'id' => 'mod-fire-danger-levels-' . $ID . '-collection',
             'classList' => ['c-collection--alarm-list'],
@@ -18,49 +18,57 @@
                 'aria-label' => __('Alarm List', 'api-alarm-integration'),
                 'aria-busy' => $isAjaxRequest ? 'true' : 'false'
             ],
-            'bordered' => true,
+            'bordered' => false,
             'unbox' => true,
         ])
             @foreach($alarm as $item)
-                @collection__item([])
+       @paper(['classList' => ['c-paper--alarm-list', 'u-margin__bottom--1']])
+
+            @collection__item([])
+
+                
 
                     @slot('prefix')
                         <div class="c-collection__icon">
-                            @icon(['icon' => $item->icon, 'size' => 'md'])
+                            @icon(['icon' => $item->icon, 'size' => 'md', 'classList' => ['u-color__text--white', 'u-color__bg--primary', 'u-rounded--8', 'u-padding--2', 'u-align-items--center', 'u-justify-content--center']])
                             @endicon
                         </div>
                     @endslot
 
                     @slot('secondary')
-                    <div data-tooltip="{{$item->date_time}}" class="u-display--flex u-align-items--center u-gap-1 u-color__text--light">   
-                        @icon(['icon' => 'alarm', 'size' => 'sm'])
-                        @endicon
-                        {{ $item->time }}
-                    </div>
+                        @typography(['element' => 'div', 'variant' => 'meta', 'attributeList' => ['data-tooltip' => $item->date_time], 'classList' => ['u-display--flex', 'u-align-items--top', 'u-gap-1', 'u-color__text--light']])
+                            {{ $item->time }}
+                        @endtypography
                     @endslot
 
                     @typography(['element' => 'h4'])
                         {!! $item->title !!}
                     @endtypography
 
+                    @typography(['element' => 'div', 'variant' => 'meta'])
+                        {{ $item->streetname }}
+                    @endtypography  
+
 
                     @if($item->moredetails)
 
-                        <ul class="u-unlist u-padding--0 u-margin__left--0 u-margin__top--1">
+                        <div class="u-padding--0 u-margin__top--1">
                             @foreach($item->moredetails as $detail)
-                            <li class="u-padding--0">
-                                @typography(['element' => 'div', 'variant' => 'meta'])
-                                Station <strong>{{ $detail->unit }}</strong> utlarmad till <strong>{{ $detail->streetname }}</strong> den <strong>{{ $detail->date_time }}</strong>.
+                                @typography(['element' => 'span', 'variant' => 'meta', 'classList' => ['u-color__bg--complementary-lighter', 'u-rounded--4', 'u-padding__x--1'], 'attributeList' => ['data-tooltip' => $detail->date_time]])
+                                    {{ $detail->unit }}
                                 @endtypography
-                            </li>
-                        @endforeach
-                        </ul>
+                            @endforeach
+                        </div>
                     @endif
+                    
+                 
 
                 @endcollection__item
+
+                   @endpaper
             @endforeach
         @endcollection
-        @endpaper
+        
 
         @typography([
             "element" => "p",
